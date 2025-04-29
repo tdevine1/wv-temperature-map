e3bdf01cd273755fefc339f9d1d6aec57348d5b2
Dr. Devine <phlosso4@gmail.com>
2025-04-29 14:47:09 -0400

    adding startup script to setup backend on app service

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,6 @@ const mysql = require('mysql2');
 console.log('Connected to ', process.env.DB_HOST + ":" + process.env.DB_PORT);
 console.log('DB_USER:', process.env.DB_USER);
 
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
